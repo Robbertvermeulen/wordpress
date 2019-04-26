12406d61b4321940c07c43bf43fe109a507f6c76
JamesGreenaway <16066511+JamesGreenaway@users.noreply.github.com>
2019-04-26 15:03:26 +0100

    added server name to script

--- config/startup.sh
+++ config/startup.sh
@@ -7,6 +7,9 @@ sed -i -e 's/^exec "$@"/#exec "$@"/g' /usr/local/bin/docker-entrypoint.sh
 # execute bash script from official wordpress image
 source /usr/local/bin/docker-entrypoint.sh
 
+# set server name 
+echo "ServerName ${APACHE_SERVER_NAME}" >> /etc/apache2/apache2.conf
+
 # custom scripts
 user.sh
 ssl.sh
