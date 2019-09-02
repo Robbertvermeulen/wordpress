8bb9048201ba3ba88f0161aeabd15c728a7b0afd
James Greenaway <greenaway.james@gmail.com>
2019-09-02 07:58:23 +0100

    delete old files

--- config/user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sed -ri "s/^www-data:x:33:33:/www-data:x:${LOCAL_UID:-1000}:${LOCAL_UID:-1000}:/" /etc/passwd
-sed -ri "s/^www-data:x:33:/www-data:x:${LOCAL_UID:-1000}:/" /etc/group
-chown -R www-data:www-data /var/www/html
