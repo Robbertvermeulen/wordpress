3b87e97515b06a7d4378d3f6c4820d6385fe7769
James Greenaway <greenaway.james@gmail.com>
2019-09-07 07:59:26 +0100

    add working support for custom vhosts

--- startup.sh
+++ startup.sh
@@ -27,11 +27,15 @@ export SERVER_NAME=`echo ${SITE_URL} |  awk -F"/" '{print $3}'`
 sudo sh -c "echo 'ServerName ${SERVER_NAME}' >> /etc/apache2/apache2.conf"
 # Change to a non-privilaged port to allow apache to be run without sudo
 sudo sed -ri "s!Listen 80!Listen 5000!" /etc/apache2/ports.conf
-# Update Virtual Hosts
+# Update Virtual Hosts if user has added custom config file
 if [ -f /tmp/*.conf ]; then
-    sudo cp /tmp/*.conf /etc/apache2/sites-available/
-    sudo chown root:root /etc/apache2/sites-available/*.conf 
+    cd /etc/apache2/sites-available
+    sudo a2dissite *
+    sudo rm 000-default.conf
+    sudo cp /tmp/*.conf .
+    sudo chown root:root *.conf 
     sudo a2ensite *
+    cd -
 else
     sudo sed -ri "s!ServerName!ServerName ${SERVER_NAME}!" /etc/apache2/sites-available/000-default.conf
 fi
